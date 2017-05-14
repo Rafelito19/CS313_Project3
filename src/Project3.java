c2e3334f9872780d6e828015bd242c927a7493c8
Rafelito19 <santana.rafael27@gmail.com>
2017-05-13 23:00:14 -0400

    latest changes

--- src/Project3.java
+++ src/Project3.java
@@ -15,7 +15,7 @@ public class Project3 {
 public static void main (String [] args ){
 	
 	
-	int numbers [][] = new int [500][500];
+	int numbers [][] = new int [10][10];
 	 File file = new File ("input.txt");
 	 try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 		    String line;
@@ -23,24 +23,25 @@ public static void main (String [] args ){
 			 
 		    int j=0;
 		    while ((line = br.readLine()) != null) {
-		   	 if (j==8){
+		   	 if (j>9){
 		   		 j=0;
+		   		 printM(numbers);
 		   		 solve_maze(numbers);
-numbers = new int [10][12];
-
+numbers = new int [10][10];
+continue;
 		   	 
 		   	 }
 		   String[] arr =line.split(" ")	;
 		    
 		    
 		   for (int i =0;i<arr.length;i++){
-			   System.out.println(arr[i]+ " i = "+i);
+			  // System.out.println(arr[i]+ " i = "+i);
 			   
 			if (arr[i]!=" ")   //do not parse spaces
 			   numbers[j][i]= Integer.parseInt(arr[i]);
 			   
 		   }
-		    
+		    //
 		    
 		 
 		    	j++;
@@ -66,16 +67,18 @@ System.out.println("invalid file provided");
 public static void solve_maze(int maze [] []){}
 
 public static void printM(int args[][])	{
-	
+	System.out.println("The maze ");
+	System.out.println();
 	for (int i =0; i <args.length;i++){
 		
 		for (int j =0; j <args[0].length;j++){
 			
-			System.out.print(args[i][j]+" |");
+			System.out.print(args[i][j]+" ");
 		}
 		
 		System.out.println();
 	}
+	System.out.println();
 }
 
 
